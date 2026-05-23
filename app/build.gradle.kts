7c226c80c31709decfc00c7aae4cd2a7e7e31f10
Dmitry Borodin <git@DmitryBorodin.com>
2026-05-23 22:29:20 +0200

    Separate debug app identity and launcher branding for debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+
         release {
             isMinifyEnabled = true
             isShrinkResources = true
