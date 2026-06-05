d584675f3f2b5d6291dd0e0cae30669d23429944
Dmitry Borodin <git@DmitryBorodin.com>
2026-06-05 19:42:25 +0200

    Release v1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "com.cloudbasepredictor"
         minSdk = 25
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.5.0"
+        versionCode = 11
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "com.cloudbasepredictor.HiltTestRunner"
 
